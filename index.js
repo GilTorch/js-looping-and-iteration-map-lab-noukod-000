fd9e446b5fe93cfbd09ca508bd1dfd86f732ebe9
Gilbert Torchon <gilbert@noukod.com>
2019-02-07 18:50:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function lowerCaseDrivers(array){
   return array.map((driver)=>driver.toLowerCase());
 }
+
+function nameToAttributes(array){
+  return array.map((name)=>Object.assign({},{firstName:name.split(" ")[0]},{lastName:name.split(" ")[1]}))
+}
